186b109d359b0f4e30f2a050339c573f0313c044
MD ABUBAKAR <abubakar.jawed@gmail.com>
2025-07-28 18:31:55 +0530

    updated url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,11 @@
 
-const url = "http://134.209.149.12"; // working in offics Replace with your actual local IP and port
+//const url = "http://134.209.149.12"; // working in offics Replace with your actual local IP and port
 
 //const url = "http://192.168.1.110:8000"; //working in PG
+
+//const url = "http://10.20.2.78:8000"; //working in PG
+
+const url = "https://rcckitportal.sun-rack.com";
 
 export const BASE_URL = `${url}/api`; 
 
